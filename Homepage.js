79ea033f6fc70a471b991e60e945981b3080a2f9
UsamDaw <148767954+UsamDaw@users.noreply.github.com>
2026-09-01 13:31:58 +0200

    Update

---
+++ Homepage.js
@@ -0,0 +1,38 @@
+
+// Tutorial for functional search bar https://www.youtube.com/watch?v=7OU4kTKV8dM
+const searchPost = () => {
+    const searchpost = document.getElementById("SearchBox").value.toUpperCase();
+    const games = document.querySelectorAll(".Game");
+    const noResults = document.getElementById("NoResults");
+
+    let found = false;
+
+    for(let p = 0; p < games.length; p++) {
+        const title = games[p].querySelector("h3");
+        const text = title.textContent.toUpperCase();
+
+        if(text.includes(searchpost)) {
+            games[p].style.display = "";
+            found = true;
+        } else {
+            games[p].style.display = "none";
+        }
+    }
+
+    if(found) {
+        noResults.style.display = "none";
+    } else {
+        noResults.style.display = "block";
+    }
+}
+
+// Show/hide contents inside the filter https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+
+function myFunction() {
+  var x = document.querySelector(".InsideFilter");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
